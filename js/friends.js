aa534c3d048ec3770ad8255131a62e05b17e6859
Nova-vm <vijaymishra2205@gmail.com>
2026-07-26 20:07:39 +0530

    1st try

---
+++ js/friends.js
@@ -0,0 +1,158 @@
+/* ===========================================================
+   Fantastic 4
+   FRIEND DATABASE
+
+   HOW TO ADD NEW FRIEND
+
+   Copy one object
+
+   Change
+
+   name
+   photo
+   thumb
+   quote
+   letter
+
+   DONE
+
+=========================================================== */
+
+const friends = [
+
+{
+
+id:1,
+
+name:"Ankita",
+
+photo:"images/friends/Ankita.jpg",
+
+thumb:"images/thumbs/Ankita.jpg",
+
+quote:"The one who can turn an ordinary day into an unforgettable memory.",
+
+letter:`
+
+Dear Ankita ❤️,
+
+Happy Friendship Day!
+
+I honestly don't know where to begin.
+
+Some friendships happen naturally.
+
+Some become unforgettable.
+
+Thank you for every laugh,
+every joke,
+every random plan,
+every memory.
+
+No matter where life takes us,
+
+Fantastic 4
+
+will always stay special.
+
+Stay exactly the way you are.
+
+❤️
+
+`
+
+},
+
+{
+
+id:2,
+
+name:"Vandana",
+
+photo:"images/friends/Vandana.jpg",
+
+thumb:"images/thumbs/Vandana.jpg",
+
+quote:"Every group needs someone who keeps everyone together.",
+
+letter:`
+
+Dear Vandana ❤️,
+
+Life changes.
+
+People change.
+
+Time changes.
+
+But real friendships never do.
+
+Thank you for being part of my journey.
+
+Happy Friendship Day ❤️
+
+`
+
+},
+
+{
+
+id:3,
+
+name:"Aman",
+
+photo:"images/friends/Aman.jpg",
+
+thumb:"images/thumbs/Aman.jpg",
+
+quote:"The person behind countless smiles.",
+
+letter:`
+
+Dear Aman ❤️,
+
+Thank you
+
+for every memory,
+
+every conversation,
+
+every moment.
+
+Happy Friendship Day ❤️
+
+`
+
+},
+
+{
+
+id:4,
+
+name:"Akash",
+
+photo:"images/friends/Akash.jpg",
+
+thumb:"images/thumbs/Akash.jpg",
+
+quote:"Some people become friends. Some become family.",
+
+letter:`
+
+Dear Akash ❤️,
+
+You're one of the reasons
+
+this group became
+
+Fantastic 4.
+
+Never change.
+
+Happy Friendship Day ❤️
+
+`
+
+}
+
+];
